3833f51a8955c4321eeef4d0f5e57c734f975012
Leo jPod <leo.jpod@lilo.org>
2019-04-03 12:25:59 +0200

    start to make a script to get a smoother install

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+####################
+# TERMINAL TOOLING #
+####################
+
+## TODO check if we're on a mac or not
+# zsh
+brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install tmux
+brew install tmux
+# install tpm
+
+## TODO check if we're on a mac or not
+# thefuck
+brew install thefuck
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+## TODO check if we're on a mac or not
+# install the search stuff
+brew install ripgrep
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+# install some node versions
+nvm install 10
+nvm install 8
+
+# install rustup
+curl https://sh.rustup.rs -sSf | sh
+
+# setup rust stuff for zsh
+rustup completions zsh > ~/.zfunc/_rustup
+
+# haskell stack
+curl -sSL https://get.haskellstack.org/ | sh
+
+
+##################
+# setup for nvim #
+##################
+
+# make sure python 3 is installed
+## TODO check if we're on a mac or not
+brew install python
+pip3 install neovim
+
+# make sure deoplete packages are installed
+
+# install bibtex and arara
+
+# install neovim stuff for ruby and node
+gem install neovim
+gem install environment
+npm i -g neovim
+
+# setup nvim directly
+nvim +PlugInstall +UpdateRemotePlugins +qa
+
