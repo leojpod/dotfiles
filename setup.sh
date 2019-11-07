ca739b84be27ba463f9417d36a157999859d87d5
Leo jPod <leo.jpod@lilo.org>
2019-11-07 16:48:59 +0100

    another try another fail

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ ln -s $CURRENT_DIR/tmux.conf $HOME/.tmux.conf
 
 rm -fr ~/.zshrc
 ln -s $CURRENT_DIR/zshrc $HOME/.zshrc
+rm -fr ~/.secrets.sh
 ln -s $CURRENT_DIR/secrets.sh $HOME/.secrets.sh
 
 rm -fr $XDG_CONFIG_HOME/kitty
@@ -23,5 +24,7 @@ ln -s $CURRENT_DIR/nvim-config $XDG_CONFIG_HOME/nvim
 
 rm -fr ~/.gitconfig
 ln -s $CURRENT_DIR/gitconfig $HOME/.gitconfig
+rm -fr ~/.git-templates
+ln -s $CURRENT_DIR/git/git-templates $HOME/.git-templates
 
 echo CONFIGURATIONS LINKED.
