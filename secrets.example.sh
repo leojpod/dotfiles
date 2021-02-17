d6aee7f1e0aa096f9860846a0ae494140c984a91
Leo jPod <leo.jpod@lilo.org>
2021-02-17 10:53:23 +0100

    add missing option in the secrets example file

--- secrets.example.sh
+++ secrets.example.sh
@@ -1,4 +1,5 @@
 export GH_USER='put your username here'
 # the link to get a token is here : https://github.com/settings/tokens
 export GH_PASS='generate an API token and put it here'
+export GPG_KEY=''
 
